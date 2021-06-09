3834e43f46c4c5aae8c891a5f888f6033db8aaeb
Doktorrea <82879041+Doktorrea@users.noreply.github.com>
2021-06-09 23:05:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*jQuery(document).ready(function($){
+  $('.navbar-toggler').click(function(){
+  $('.add-dropdown').attr("data-toggle", "dropdown"); 
+  });
+});*/
+  
+  
+  AOS.init({
+    duration: 1800,
+    easing: 'slide',
+    once: false
+  });
